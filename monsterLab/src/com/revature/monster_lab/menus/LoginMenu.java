9587a0d6f8f7f50cdfc4fafa6cf8f463d491afa9
JesterCharles <charles.jester@revature.com>
2022-01-28 12:54:35 -0500

    Added monsterLab menus, Welcome only one with implementation ready.

---
+++ monsterLab/src/com/revature/monster_lab/menus/LoginMenu.java
@@ -0,0 +1,21 @@
+package com.revature.monster_lab.menus;
+
+import java.io.BufferedReader;
+
+import com.revature.monster_lab.util.MenuRouter;
+
+public class LoginMenu extends Menu {
+
+
+	public LoginMenu(BufferedReader consoleReader, MenuRouter router) {
+		super("Login", "/login", consoleReader, router);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ monsterLab/src/com/revature/monster_lab/menus/WelcomeMenu.java
@@ -0,0 +1,42 @@
+package com.revature.monster_lab.menus;
+
+import java.io.BufferedReader;
+
+import com.revature.monster_lab.util.MenuRouter;
+import static com.revature.monster_lab.util.AppState.shutdown;
+
+public class WelcomeMenu extends Menu{
+
+
+
+	public WelcomeMenu(BufferedReader consoleReader, MenuRouter router) {
+		super("Welcome", "/welcome", consoleReader, router);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render() throws Exception {
+		
+		System.out.print(
+				"Welcome to the Mosnter Laboratory!\n" + "1) Login\n" + "2) Register\n" + "3) Exits\n" + "> ");
+		
+		String userSelection = consoleReader.readLine();
+
+		switch (userSelection) {
+		case "1":
+			router.transfer("/login");
+			break;
+		case "2":
+			router.transfer("/register");
+			break;
+		case "3":
+			shutdown();
+			break;
+		default:
+			System.out.println("What on earth are you trying to tell me to do?!?!");
+			break;
+		}
+		
+	}
+
+}
