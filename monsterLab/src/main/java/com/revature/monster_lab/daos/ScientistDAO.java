4d858cdc5be16c9be7d3e6a704df5a8f9b636b77
JesterCharles <charles.jester@revature.com>
2022-02-02 17:07:48 -0500

    Added the JDBC connectivity to our DAO, and updatd the services to call new DAO implementations

--- monsterLab/src/main/java/com/revature/monster_lab/daos/ScientistDAO.java
+++ monsterLab/src/main/java/com/revature/monster_lab/daos/ScientistDAO.java
@@ -1,21 +1,19 @@
 package com.revature.monster_lab.daos;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.UUID;
 
 import com.revature.monster_lab.models.Scientist;
 import com.revature.monster_lab.util.ConnectionFactory;
+import com.revature.monster_lab.util.LinkedList;
 import com.revature.monster_lab.util.List;
 
 public class ScientistDAO implements CrudDAO<Scientist> {
 
-	File scientistsFile = new File("src/main/resources/data.txt");
 
 	// TODO: Implement Authentication
 	public Scientist findByUsernameAndPassword(String username, String password) {
@@ -29,23 +27,81 @@ public class ScientistDAO implements CrudDAO<Scientist> {
 		
 		return null;
 	}
-
-	@Override
-	public Scientist create(Scientist newScientist) {
-
-		try (FileWriter fileWriter = new FileWriter(scientistsFile, true);) {
-			newScientist.setScientistId(UUID.randomUUID().toString());
-			fileWriter.write(newScientist.toFileString() + "\n");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("Error persistin user to file");
-		}
+	
+	// TODO: Implement FindByEmail
+	public Scientist findByEmail(String email) {
+		return null;
+	}
+	
+	// TODO: Implement FindByUsername
+	public Scientist findByUsername(String username) {
 		return null;
 	}
 
 	@Override
+	public Scientist create(Scientist newScientist) {
+
+		try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			
+			newScientist.setScientistId(UUID.randomUUID().toString());
+			
+			String sql = "insert into scientists (scientist_id, first_name, last_name, email, username, password) values (?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, newScientist.getScientistId());
+			ps.setString(2, newScientist.getFirstName());
+			ps.setString(3, newScientist.getLastName());
+			ps.setString(4, newScientist.getEmail());
+			ps.setString(5, newScientist.getUsername());
+			ps.setString(6, newScientist.getPassword());
+			
+			int rowsInserted = ps.executeUpdate();
+			
+			if(rowsInserted != 0) {
+				return newScientist;
+			}
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return null;
+	
+	}
+
+	@Override
 	public List<Scientist> findAll() {
-		// TODO Auto-generated method stub
+		
+		List<Scientist> scientistsList = new LinkedList<>(); 
+		
+		try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			String sql = "select * from scientists";
+			Statement s = conn.createStatement();
+			
+			ResultSet resultSet = s.executeQuery(sql);
+			
+			
+			while(resultSet.next()) {
+				Scientist scientist = new Scientist();
+				scientist.setScientistId(resultSet.getString("scientist_id"));
+				scientist.setFirstName(resultSet.getString("first_name"));
+				scientist.setLastName(resultSet.getString("last_name"));
+				scientist.setEmail(resultSet.getString("email"));
+				scientist.setUsername(resultSet.getString("username"));
+				scientist.setPassword(resultSet.getString("password"));
+				
+				scientistsList.add(scientist);
+			}	
+			
+			return scientistsList;
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
