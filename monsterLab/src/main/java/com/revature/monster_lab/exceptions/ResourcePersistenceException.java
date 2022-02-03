57ce4446afcc23883b8eec4617786983ed445558
JesterCharles <charles.jester@revature.com>
2022-02-03 13:14:16 -0500

    Included an authentication and resource persistence exception

---
+++ monsterLab/src/main/java/com/revature/monster_lab/exceptions/ResourcePersistenceException.java
@@ -0,0 +1,10 @@
+package com.revature.monster_lab.exceptions;
+
+public class ResourcePersistenceException extends RuntimeException {
+
+	public ResourcePersistenceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
