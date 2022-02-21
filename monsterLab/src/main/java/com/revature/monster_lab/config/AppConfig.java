be4c9718f613973b8de36bbf41d8c3405ba24ef5
JesterCharles <charles.jester@revature.com>
2022-02-21 01:00:34 -0500

    Set up the basic AppConfig for our monsterLab to now function as a Component scan Spring Project

---
+++ monsterLab/src/main/java/com/revature/monster_lab/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.revature.monster_lab.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.revature")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AppConfig {
+
+}
