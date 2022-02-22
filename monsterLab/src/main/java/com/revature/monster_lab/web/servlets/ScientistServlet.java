830ab67b1e07bcf970412b056f7ba3e4313f1e88
JesterCharles <charles.jester@revature.com>
2022-02-22 10:37:38 -0500

    Updated the response status for registering a scientist

--- monsterLab/src/main/java/com/revature/monster_lab/web/servlets/ScientistServlet.java
+++ monsterLab/src/main/java/com/revature/monster_lab/web/servlets/ScientistServlet.java
@@ -1,9 +1,11 @@
 package com.revature.monster_lab.web.servlets;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.monster_lab.services.ScientistService;
@@ -21,6 +23,7 @@ public class ScientistServlet {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public void createScientist(@RequestBody ScientistRequest scientistRequest) {
 		scientistService.registerNewScientist(scientistRequest);
 	}
