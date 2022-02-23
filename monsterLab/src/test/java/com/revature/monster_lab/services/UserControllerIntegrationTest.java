9a35090d6e3fcd868bb78f1330223c513766312c
JesterCharles <charles.jester@revature.com>
2022-02-23 02:16:25 -0500

    Updated the pom to include the azure container registry

--- monsterLab/src/test/java/com/revature/monster_lab/services/UserControllerIntegrationTest.java
+++ monsterLab/src/test/java/com/revature/monster_lab/services/UserControllerIntegrationTest.java
@@ -1,18 +1,10 @@
 package com.revature.monster_lab.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 public class UserControllerIntegrationTest {
